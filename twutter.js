4ac77cf972f7ffa37a34f57c1942bf8e0f8513d7
Jordan <jabyrd3@gmail.com>
2014-05-18 19:58:35 -0400

    init commit

---
+++ twutter.js
@@ -0,0 +1,31 @@
+var Twit = require('twit');
+
+var T = require('./config1.js');
+var ping = new Twit(T);
+var cursor = undefined;
+var cursorCount = 0;
+var getWithCursor = function(cursor){        
+    if(cursorCount < 3){
+        ping.get('statuses/user_timeline', { screen_name: '@profdavidflint', max_id: cursor, count: 200 },  function (err, reply) {
+            for(x = 0; x < reply.length; x++){
+                console.log(reply[x].text); 
+                if(x === reply.length - 1){
+                    cursor = reply[x].id - 3;
+                    getWithCursor(cursor);
+                }
+            }
+            cursorCount++;
+        });
+    }
+}
+
+ping.get('statuses/user_timeline', { screen_name: '@profdavidflint', count: 200 },  function (err, reply) {
+    for(x = 0; x < reply.length; x++){
+        console.log(reply[x].text);
+        if(x === reply.length - 1){
+            cursor = reply[x].id - 3;
+            getWithCursor(cursor);
+        }
+    }
+    cursorCount++;
+})
